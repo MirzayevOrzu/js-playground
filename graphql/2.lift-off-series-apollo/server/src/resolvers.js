da09d4282bbf4e43d0896642f68a54ee3f2b0639
MirzayevOrzu <mirzayevorzu127@gmail.com>
2022-07-07 16:15:09 +0500

    Learn apollo graphql arguments: Lift Off Series

--- graphql/2.lift-off-series-apollo/server/src/resolvers.js
+++ graphql/2.lift-off-series-apollo/server/src/resolvers.js
@@ -4,10 +4,20 @@ const resolvers = {
     getTracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackApi.getTracksForHome();
     },
+    // returns a single track by id
+    getTrack: (_, { id }, { dataSources }) => {
+      return dataSources.trackApi.getTrack(id);
+    },
+    getTrackModules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackApi.getTrackModules(id);
+    },
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackApi.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackApi.getTrackModules(id);
     },
   },
 };
--- graphql/2.lift-off-series-apollo/server/src/schema.js
+++ graphql/2.lift-off-series-apollo/server/src/schema.js
@@ -3,7 +3,9 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     "Query to get Tracks for home page"
-    getTracksForHome: [Track!]!                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    getTracksForHome: [Track!]!
+    getTrack(id: ID!): Track
+    getTrackModules(id: ID!): [Module]
   }
 
   "A Track is a group of Modules that teach about a specific topic"
@@ -19,6 +21,18 @@ const typeDefs = gql`
     length: Int
     "Represents how many Modules are there in A Track"
     modulesCount: Int
+    "Description of track"
+    description: String
+    "How many times a track has been viewed"
+    numberOfViews: Int
+    "Tracks complete array of Modules"
+    modules: [Module!]!
+  }
+
+  type Module {
+    id: ID!
+    title: String!
+    length: Int
   }
 
   "Author of a Track"
